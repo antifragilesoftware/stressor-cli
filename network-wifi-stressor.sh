5a9f013bb1e3f1553f1cf0f823f0ef8a5e9e6a97
Russ Miles <russ@russmiles.com>
2017-06-28 23:06:46 +0100

    Added parameter inspection to bash script

--- network-wifi-stressor.sh
+++ network-wifi-stressor.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo Parameters
+echo $stressor
+
 # get current wifi device
 CURRENT_DEVICE=$(networksetup -listallhardwareports | awk '$3=="Wi-Fi" {getline; print $2}')
 echo "Current Wi-Fi Device = '$CURRENT_DEVICE'"
